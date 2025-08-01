c4941b8499a78b6e2d1308e490c393e7427cf84f
Tejas <tejasbhalerao85@gmail.com>
2025-08-01 16:33:12 +0530

    cros

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,7 @@ const app = express();
 
 // --- Global Middleware ---
 app.use(cors({
-  origin:["http://localhost:5173"]
+  origin:["http://localhost:5173","https://turf-booking-system-tejasdev.vercel.app"]
 }));
 app.use(helmet());
 app.use(express.json({ limit: '2mb' }));
